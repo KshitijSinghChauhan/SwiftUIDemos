2ad3fdba776af9a2e5adb8417a348ec98ebdf020
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2025-05-25 16:03:29 +0530

    added Restart app

---
+++ 6Restart/restart-swiftui/Screens/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  restart-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
