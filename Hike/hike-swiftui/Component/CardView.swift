6fad5ce1bd0975f40de8ea0997e68c0f97870962
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2025-05-25 15:54:11 +0530

    added Hike app

---
+++ Hike/hike-swiftui/Component/CardView.swift
@@ -0,0 +1,106 @@
+//
+//  CardView.swift
+//  hike-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 04/12/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+
+    // MARK: - PROPERTIES
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    @State private var isShowingSheet: Bool = false
+
+    // MARK: - FUNCTIONS
+
+    func randomImage() {
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber
+        imageNumber = randomNumber
+    }
+    var body: some View {
+        ZStack {
+            CustomBackgroundView()
+            VStack {
+                // MARK: HEADER
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [
+                                        .customGrayLight,
+                                        .customGrayMedium
+                                    ],
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                        )
+                        Spacer()
+                        Button {
+                            // ACTION: Show a sheet
+                            print("The button was pressed.")
+                            isShowingSheet.toggle()
+                        } label: {
+                            CustomButtonView()
+                        }
+                        .sheet(isPresented: $isShowingSheet) {
+                            SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
+                        }
+                    }
+                    Text("Fun and enjoyable activities for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal, 30)
+
+                // MARK: MAIN CONTENT
+                ZStack {
+                    CustomCircleView()
+
+                    Image("image-\(imageNumber)")
+                        .resizable()
+                        .scaledToFit()
+                        .animation(.default, value: imageNumber)
+                }
+
+                // MARK: FOOTER
+                Button {
+                    // ACTION: Generate a random number
+                    print("The button was pressed.")
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [
+                                    .customGreenLight,
+                                    .customGreenMedium],
+                                startPoint: .top,
+                                endPoint: .bottom)
+                        )
+                        .shadow(color: .black.opacity(0.25), radius: 0.25,x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
+            } //: ZStack
+        } //: CARD
+        .frame(width: 320, height: 570)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
---
+++ Hike/hike-swiftui/Screen/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  hike-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 04/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
